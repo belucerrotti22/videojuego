f16ef5ca129c0428f93c15e5d7d5a862bbb9d4ef
belucerrotti22 <belencerrotti@gmail.com>
2023-03-05 19:05:19 -0300

    se puede pasar de nivel, se puede reintentar cuando se pierde y se puede volver a ganar cuando se gana

--- main.js
+++ main.js
@@ -5,15 +5,24 @@ const abajoButton = document.querySelector('#abajo');
 const derechaButton = document.querySelector('#derecha');
 const izquierdaButton = document.querySelector('#izquierda');
 const cartelGameOver = document.querySelector('#game-over');
+const cartelGameWon = document.querySelector('#game-won');
+const buttonTryAgain = document.querySelector('#try-again');
+const buttonPlayAgain = document.querySelector('#play-again');
 
 const player = 'PLAYER';
 const explotion = 'BOMB_COLLISION';
+const bomb = 'X';
+const win = 'WIN';
+const gift = 'I';
 const door = 'O';
+const firstLevel = 0;
+const secondLevel = 1;
+const finalLevel = 2;
 let isGameOver = false;
 let elementSize;
 let canvaSize;
-let actualPosition = [undefined, undefined];
-let nivelActual = 1;
+let actualPos = [undefined, undefined];
+let nivelActual = firstLevel;
 let mapaActual = [];
 
 window.addEventListener('load', resize);
@@ -22,6 +31,21 @@ arribaButton.addEventListener('click', moveUp);
 abajoButton.addEventListener('click', moveDown);
 derechaButton.addEventListener('click', moveRight);
 izquierdaButton.addEventListener('click', moveLeft);
+buttonTryAgain.addEventListener('click', restartGame);
+buttonPlayAgain.addEventListener('click', restartGame);
+
+function restartGame(){
+    if (isGameOver){
+        isGameOver = false;
+        cartelGameOver.classList.toggle('inactive');
+    }else {
+        cartelGameWon.classList.toggle('inactive');
+    }
+    nivelActual = firstLevel;
+    actualPos = [undefined, undefined];
+    clearAll();
+    startGame();
+}
 
 function resize(){
 
@@ -68,10 +92,13 @@ function llenarMapa(){
     for (let i = 0; i < 10; i++){
         for (let j = 0; j < 10; j++){
             let space = mapaActual[j][i];
-            if (actualPosition[0] == undefined && space == door){
-                actualPosition[0] = elementSize * i;
-                actualPosition[1] = elementSize * (j + 1);
-                game.fillText(emojis[player], actualPosition[0], actualPosition[1]);
+            if (actualPos[0] == undefined && space == door){
+                actualPos[0] = elementSize * i;
+                actualPos[1] = elementSize * (j + 1);
+                game.fillText(emojis[player], actualPos[0], actualPos[1]);
+            }else if (space == gift && nivelActual == finalLevel) {
+                mapaActual[j][i] = win;
+                game.fillText(emojis[win], elementSize * i, elementSize * (j + 1));
             }else {
                 game.fillText(emojis[space], elementSize * i, elementSize * (j + 1));
             }   
@@ -82,26 +109,36 @@ function llenarMapa(){
 
 function clear(){
     game.fillStyle = "rgb(131, 154, 161)";
-    game.fillRect(actualPosition[0] + 6, actualPosition[1] - 43, 50, 51);
+    game.fillRect(actualPos[0] + 6, actualPos[1] - 41, 45, 50);
+}
+
+function clearAll(){
+    game.fillStyle = "rgb(131, 154, 161)";
+    game.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 
-function isPossible(x, y){
-    if(mapaActual[y][x] == 'X'){
-        mapaActual[y][x] = explotion;
-        llenarMapa();
-        game.fillText(emojis[player], actualPosition[0], actualPosition[1]);
-        return false;
-    }
-    return true;
+function isThereABomb(x, y){
+    return mapaActual[y][x] == bomb;
+}
+
+function isGameWon(x, y){
+    return mapaActual[y][x] == win;
+}
+
+function isLevelWon(x, y){
+    return mapaActual[y][x] == gift;
 }
 
 function coordenadas(position){
     return [(position[0] / elementSize), (position[1] / elementSize) - 1];
 }
 
-function gameOver(){
+function gameOver(x, y){
     isGameOver = true;
+    mapaActual[y][x] = explotion;
+    llenarMapa();
+    game.fillText(emojis[player], actualPos[0], actualPos[1]);
     setTimeout(() => {
         cartelGameOver.classList.toggle('inactive');
     }, 300);
@@ -109,60 +146,88 @@ function gameOver(){
 
 function moveUp(){
     if (!isGameOver){
-        clear();
-        let actualCoordenadas = coordenadas(actualPosition);
-        console.log(actualCoordenadas);
-        if (isPossible(actualCoordenadas[0], actualCoordenadas[1] - 1)){
-            actualPosition[1] = elementSize * (actualCoordenadas[1]);
-            startGame();
-            game.fillText(emojis[player], actualPosition[0], actualPosition[1]);
+        let actualCoords = coordenadas(actualPos);
+        let newX = actualCoords[0];
+        let newY = actualCoords[1] - 1;
+        if (isThereABomb(newX, newY)){
+            gameOver(newX, newY);
+        }else if (isGameWon(newX, newY)) {
+            cartelGameWon.classList.toggle('inactive');
         }else {
-            gameOver();
+            if (isLevelWon(newX, newY)){
+                nivelActual++;
+                clearAll();
+            }
+            clear();
+            actualPos[1] = elementSize * (actualCoords[1]);
+            startGame();
+            game.fillText(emojis[player], actualPos[0], actualPos[1]);
         }
     }
 }
 
 function moveDown(){
     if (!isGameOver){
-        clear();
-        let actualCoordenadas = coordenadas(actualPosition);
-        console.log(actualCoordenadas);
-        if (isPossible(actualCoordenadas[0], actualCoordenadas[1] + 1)){
-            actualPosition[1] = elementSize * (actualCoordenadas[1] + 2);
-            startGame();
-            game.fillText(emojis[player], actualPosition[0], actualPosition[1]);
+        let actualCoords = coordenadas(actualPos);
+        let newX = actualCoords[0];
+        let newY = actualCoords[1] + 1;
+        if (isThereABomb(newX, newY)){
+            gameOver(newX, newY);
+        }else if (isGameWon(newX, newY)) {
+            cartelGameWon.classList.toggle('inactive');
         }else {
-            gameOver();
+            if (isLevelWon(newX, newY)){
+                nivelActual++;
+                clearAll();
+            }
+            clear();
+            actualPos[1] = elementSize * (actualCoords[1] + 2);
+            startGame();
+            game.fillText(emojis[player], actualPos[0], actualPos[1]);
         }
     }
 }
 
 function moveRight(){
     if (!isGameOver){
-        clear();
-        let actualCoordenadas = coordenadas(actualPosition);
-        console.log(actualCoordenadas);
-        if (isPossible(actualCoordenadas[0] + 1, actualCoordenadas[1])){
-            actualPosition[0] = elementSize * (actualCoordenadas[0] + 1);
-            startGame();
-            game.fillText(emojis[player], actualPosition[0], actualPosition[1]);
+        let actualCoords = coordenadas(actualPos);
+        let newX = actualCoords[0] + 1;
+        let newY = actualCoords[1];
+        if (isThereABomb(newX, newY)){
+            gameOver(newX, newY);
+        }else if (isGameWon(newX, newY)) {
+            cartelGameWon.classList.toggle('inactive');
         }else {
-            gameOver();
+            if (isLevelWon(newX, newY)){
+                nivelActual++;
+                clearAll();
+            }
+            clear();
+            actualPos[0] = elementSize * (actualCoords[0] + 1);
+            startGame();
+            game.fillText(emojis[player], actualPos[0], actualPos[1]);
         }
     }
 }
 
 function moveLeft(){
     if (!isGameOver){
-        clear();
-        let actualCoordenadas = coordenadas(actualPosition);
-        console.log(actualCoordenadas);
-        if (isPossible(actualCoordenadas[0] - 1, actualCoordenadas[1])){
-            actualPosition[0] = elementSize * (actualCoordenadas[0] - 1);
-            startGame();
-            game.fillText(emojis[player], actualPosition[0], actualPosition[1]);
+        let actualCoords = coordenadas(actualPos);
+        let newX = actualCoords[0] - 1;
+        let newY = actualCoords[1];
+        if (isThereABomb(newX, newY)){
+            gameOver(newX, newY);
+        }else if (isGameWon(newX, newY)) {
+            cartelGameWon.classList.toggle('inactive');
         }else {
-            gameOver();
+            if (isLevelWon(newX, newY)){
+                nivelActual++;
+                clearAll();
+            }
+            clear();
+            actualPos[0] = elementSize * (actualCoords[0] - 1);
+            startGame();
+            game.fillText(emojis[player], actualPos[0], actualPos[1]);
         }
     }
 }
